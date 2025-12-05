a2f699cc2beba8fd363095950a818db8dfc0c504
super <97206982+SuperMo0@users.noreply.github.com>
2025-12-05 10:00:00 +0200

    add backend core logic

---
+++ BACKEND/routes/auth.router.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import * as controller from './../controllers/auth.controller.js'
+
+
+
+
+const router = Router();
+
+
+router.post('/login', controller.login);
+
+
+router.post('/signup', controller.signup);
+
+router.post('/logout', controller.logout);
+
+router.get('/check', controller.check);
+
+
+export default router
+
+
+
