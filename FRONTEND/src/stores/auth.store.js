f1b8e48d04b42d94380bf7ab166d8b8162e0eaa5
super <97206982+SuperMo0@users.noreply.github.com>
2025-12-10 10:00:00 +0200

    add frontend core logic and state management

---
+++ FRONTEND/src/stores/auth.store.js
@@ -0,0 +1,73 @@
+import { create } from "zustand"
+import api from '../lib/axios.js'
+import { toast } from 'react-toastify';
+
+
+export const useAuthStore = create((set, get) => ({
+
+    authUser: null,
+
+    isSigningIn: false,
+
+    isChecking: true,
+
+    socket: null,
+
+    isSigningUp: false,
+
+
+    check: async () => {
+        try {
+            let res = await api.get('/auth/check');
+            set({ authUser: res.data.user })
+        } catch (error) {
+            if (error.code == "ERR_NETWORK")
+                toast.error('we are having a problem connecting to servers please try again later');
+
+
+        }
+        finally {
+            set({ isChecking: false });
+        }
+    },
+
+    login: async (data) => {
+        set({ isSigningIn: true })
+        try {
+            let res = await api.post('/auth/login', data)
+            set({ authUser: res.data.user })
+        } catch (error) {
+            if (error.code == "ERR_NETWORK")
+                toast.error('we are having a problem connecting to servers please try again later');
+            else {
+                let message = error.response.data.message;
+                toast.error(message);
+            }
+        }
+        finally {
+            set({ isSigningIn: false });
+            set({ isChecking: false });
+        }
+    },
+
+    signup: async (data) => {
+        set({ isSigningUp: true })
+        try {
+            let result = await api.post('/auth/signup', data);
+            const user = result.data.user;
+            set({ authUser: user });
+        } catch (error) {
+            toast.error(error?.response?.data?.message);
+        }
+        finally {
+            set({ isSigningUp: false });
+            set({ isChecking: false });
+        }
+    },
+
+    logout: async () => {
+        await api.post('/auth/logout');
+        set({ authUser: null });
+    }
+}))
+
